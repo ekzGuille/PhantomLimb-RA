9bcc424b443abdb563582d52b2b940d3cf0349f5
ekzGuille <guillermosesesantos@hotmail.com>
2019-01-10 12:51:40 +0100

    añadido .gitignore

--- Arduino/Leer-EMG-Sensor/Leer-EMG-Sensor.ino
+++ Arduino/Leer-EMG-Sensor/Leer-EMG-Sensor.ino
@@ -5,15 +5,15 @@
 
 #include "SPI.h"
 
+//Pin en el que se ha conectado el sensor
 int pin = A0;
-//int x = 0;
 
-void setup() {
+void setup(){
   //Baud rates:300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, or 115200
   Serial.begin(9600);
 }
 
-void loop() {
+void loop(){
   Serial.println(analogRead(pin));
   delay(1);
 }
