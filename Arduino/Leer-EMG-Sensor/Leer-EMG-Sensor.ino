b6b4cfd068259bcd9a1a80c5205cf1aa069a9fc5
Guillermo Sesé Santos <guillermosesesantos@hotmail.com>
2019-02-18 21:48:46 +0100

    Tests hechos

--- Arduino/Leer-EMG-Sensor/Leer-EMG-Sensor.ino
+++ Arduino/Leer-EMG-Sensor/Leer-EMG-Sensor.ino
@@ -5,7 +5,7 @@
 
 
 //Pin en el que se ha conectado el sensor
-int inputPin = A0;
+int inputPin = A5;
 
 void setup(){
   //Baud rates:300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, or 115200
@@ -15,31 +15,30 @@ void setup(){
 void loop(){
   Serial.println(analogRead(inputPin));
   
-  delay(1);
-}
-/*
-int onboardLED = 13;            // Arduino onboard LED (pin 13) you can control
-int voltageThreshold = 400;     // any reading higher will trigger an action
-
-void setup() {
-  // put your setup code here, to run when Arduino is powered on:
-  Serial.begin(9600);
-  pinMode(onboardLED, OUTPUT);
+  delay(10);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int currentVoltage = analogRead(A0);  // store the incoming voltage 
-  Serial.print(currentVoltage);         // prints voltage to monitor
-  
-  if(currentVoltage > voltageThreshold){
-     // trigger actions
-     Serial.println(" CONTRACTION!"); // prints string + new line
-     digitalWrite(onboardLED, HIGH);  //this sends 5V, turning on LED
-  } else {
-      Serial.println("");             //adds a new line
-     digitalWrite(onboardLED, LOW); 
-     // turn off the light as threshold is not met
-  }
-}
-*/
+//int onboardLED = 13;            // Arduino onboard LED (pin 13) you can control
+//int voltageThreshold = 300;     // any reading higher will trigger an action
+//
+//void setup() {
+//  // put your setup code here, to run when Arduino is powered on:
+//  Serial.begin(9600);
+//  pinMode(onboardLED, OUTPUT);
+//}
+//
+//void loop() {
+//  // put your main code here, to run repeatedly:
+//  int currentVoltage = analogRead(A0);  // store the incoming voltage 
+//  Serial.print(currentVoltage);         // prints voltage to monitor
+//  
+//  if(currentVoltage > voltageThreshold){
+//     // trigger actions
+//     Serial.println(" CONTRACTION!"); // prints string + new line
+//     digitalWrite(onboardLED, HIGH);  //this sends 5V, turning on LED
+//  } else {
+//      Serial.println("");             //adds a new line
+//     digitalWrite(onboardLED, LOW); 
+//     // turn off the light as threshold is not met
+//  }
+//}
