c3a50c12d561d7dc8a5bc061c964bce0197f69b7
Guillermo Sesé Santos <guillermosesesantos@hotmail.com>
2019-04-19 17:25:39 +0200

    Interconexión realizada Closes #1

--- Unity/PhantomLimb-RA/Assets/Scripts/ArduinoMovement.cs
+++ Unity/PhantomLimb-RA/Assets/Scripts/ArduinoMovement.cs
@@ -5,13 +5,16 @@ using System.IO.Ports;
 
 public class ArduinoMovement : MonoBehaviour
 {
+    private const string WINDOWS_PORT = "COM";
+    private const string MAC_PORT = "/dev/tty.usbmodem14101";
+    private const int PORT = 9600;
 
     private const float MIN_MOV = 0.0f;
     private const float MAX_MOV = 400.0f;
     private float speed = 0.1f;
     private float movement;
 
-    SerialPort serial = new SerialPort("COM3", 9600); // Puerto, baudios
+    SerialPort serial = new SerialPort(MAC_PORT, PORT); // Puerto, baudios
 
     // Start is called before the first frame update
     void Start()
@@ -23,26 +26,23 @@ public class ArduinoMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         movement = speed * Time.deltaTime;
         if (serial.IsOpen)
         {
             try
             {
-                MoveLeg(serial.ReadByte());
-                print(serial.ReadByte());
+                MoveLeg(float.Parse(serial.ReadLine()));
                 Debug.Log("Esto es la lectura del arduino : " + serial.ReadLine());
             }
             catch (System.Exception)
             {
-
             }
         }
     }
 
     void MoveLeg(float legMovement)
     {
-        //transform.Translate(Vector3.left * movement, Space.World);
         transform.Translate(Vector3.left * legMovement, Space.World);
 
     }
