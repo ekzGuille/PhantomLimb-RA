5877b26575138c38cf0c10fd93903332314852db
Ag3nte <albertoalbertomax@hotmail.com>
2019-06-05 21:47:59 +0200

    #14 - Arreglado Cambio de color al ajuste de la pierna

--- Unity/PhantomLimb-RA/Assets/Scripts/MENU/SkinController.cs
+++ Unity/PhantomLimb-RA/Assets/Scripts/MENU/SkinController.cs
@@ -19,53 +19,8 @@ public class SkinController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        slider.onValueChanged.AddListener((value) =>
-        {
-            switch (value)
-            {
-                case 1:
-                    // Claro
-                    if(dropdown.captionText.text == "Pierna Derecha")
-                    {
-                        piernaR.GetComponent<Renderer>().material = m_skin_light;
+        slider.onValueChanged.AddListener((value) => ChangeSkinLeg(value));
 
-                    }else
-                    {
-                        piernaL.GetComponent<Renderer>().material = m_skin_light;
-
-                    }
-                    break;
-
-                case 2:
-                    // Normal
-                    if (dropdown.captionText.text == "Pierna Derecha")
-                    {
-                        piernaR.GetComponent<Renderer>().material = m_skin;
-
-                    }
-                    else
-                    {
-                        piernaL.GetComponent<Renderer>().material = m_skin;
-
-                    }
-
-                    break;
-                case 3:
-                    // Oscura
-                    if (dropdown.captionText.text == "Pierna Derecha")
-                    {
-                        piernaR.GetComponent<Renderer>().material = m_skin_dark;
-
-                    }
-                    else
-                    {
-                        piernaL.GetComponent<Renderer>().material = m_skin_dark;
-
-                    }
-                    break;
-            }
-
-        });
         // m_skin = Resources.Load("m_skin", typeof(Material)) as Material;
         // m_skin_light = Resources.Load("m_skin_light", typeof(Material)) as Material;
         // m_skin_dark = Resources.Load("m_skin_dark", typeof(Material)) as Material;
@@ -77,6 +32,54 @@ public class SkinController : MonoBehaviour
     void Update()
     {
 
+    }
+
+    void ChangeSkinLeg(float value)
+    {
+        switch (value)
+        {
+            case 1:
+                // Claro
+                if (dropdown.captionText.text == "Pierna Derecha")
+                {
+                    piernaR.GetComponent<Renderer>().material = m_skin_light;
+
+                }
+                else
+                {
+                    piernaL.GetComponent<Renderer>().material = m_skin_light;
+
+                }
+                break;
+
+            case 2:
+                // Normal
+                if (dropdown.captionText.text == "Pierna Derecha")
+                {
+                    piernaR.GetComponent<Renderer>().material = m_skin;
+
+                }
+                else
+                {
+                    piernaL.GetComponent<Renderer>().material = m_skin;
+
+                }
+
+                break;
+            case 3:
+                // Oscura
+                if (dropdown.captionText.text == "Pierna Derecha")
+                {
+                    piernaR.GetComponent<Renderer>().material = m_skin_dark;
+
+                }
+                else
+                {
+                    piernaL.GetComponent<Renderer>().material = m_skin_dark;
+
+                }
+                break;
+        }
     }
 
 
